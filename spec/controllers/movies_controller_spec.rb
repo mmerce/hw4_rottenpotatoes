a93059ab6f982cc9b288f26faf6a2f7718ca2ae8
mmerce <mmartin@atinet.es>
2012-03-28 16:04:56 +0000

    more stubs to spec

--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -3,13 +3,17 @@ describe MoviesController do
   describe 'similar movies' do
     before :each do
       @fake_results = [mock('Movie'), mock('Movie')]
+      @fake_movie = mock('Movie')
+      @fake_movie.stub(:title => 'the film')
     end
     it 'should call the model method that selects similar movies by id' do
+      Movie.should_receive(:find).and_return(@fake_movie)
       Movie.should_receive(:similar_by_director).with(1).and_return(@fake_results)
       post :similar, {:id => "1"}
     end
     describe 'after valid search' do
       before :each do
+        Movie.should_receive(:find).and_return(@fake_movie)
         Movie.should_receive(:similar_by_director).with(1).and_return(@fake_results)
         post :similar, {:id => "1"}
       end
